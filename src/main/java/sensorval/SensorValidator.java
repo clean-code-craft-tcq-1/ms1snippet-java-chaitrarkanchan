735d9901961d4dfbe6e944aab4dbbc4f2e67eb67
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-03-24 14:13:55 +0530

    Update SensorValidator.java

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -4,25 +4,25 @@ import java.util.List;
 
 public class SensorValidator 
 {
-    public static boolean _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
+	static final double SOC_JUMPFREQUENCY=0.05;
+	static final double  CURRENT_JUMPFREQUENCY=0.1;
+	
+    public static boolean isSensorNoisy(double value, double nextValue, double maxDelta) {
+    	
+        return (nextValue - value > maxDelta);
     }
+    
     public static boolean validateSOCreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.05)) {
-            return false;
-            }
-        }
-        return true;
+    	return (values.isEmpty()&&values==null)?false:validateReadings(values,SOC_JUMPFREQUENCY);
     }
     public static boolean validateCurrentreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
+        return (values.isEmpty()&&values==null)?false:validateReadings(values,CURRENT_JUMPFREQUENCY);
+    }
+    
+    public static boolean validateReadings(List<Double> values,double jumpfrequency){
+    	int lastButOneIndex = values.size() - 1;
         for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.1)) {
+            if(isSensorNoisy(values.get(i), values.get(i + 1),jumpfrequency)) {
             return false;
             }
         }
